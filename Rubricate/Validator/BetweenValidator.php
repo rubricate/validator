47cb464b27e54181a34210d2f959fa1dd42bd321
Estefanio N Santos <estefanions@gmail.com>
2024-03-19 22:35:43 -0300

    HOTFIX - Coding in 'BetweenValidator' constructor

--- Rubricate/Validator/BetweenValidator.php
+++ Rubricate/Validator/BetweenValidator.php
@@ -4,12 +4,13 @@ namespace Rubricate\Validator;
 
 class BetweenValidator implements IIsValidValidator
 {
-    private $v;
+    private $v, $encoding;
 
-    public function __construct($keyMinAndMaxArr)
+    public function __construct($keyMinAndMaxArr, $encoding = 'UTF-8')
     {
         $this->v = new VoValidator();
         $this->v->setRule($keyMinAndMaxArr);
+        $this->encoding = $encoding;
     }
 
     public function isValid($field)
